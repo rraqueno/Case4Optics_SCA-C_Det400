0720eb007b255442cd5a0bbc1b1a7f9b2b6be17b
U-homestar\rvrpci <rvrpci@homestar.(none)>
2014-06-24 14:19:56 -0400

    Added screencast of this process.

--- link_files.bash
+++ link_files.bash
@@ -3,5 +3,5 @@ cp -uv MODIS/MOD_A2013-158-1745-geoL8TOA_B11.hdr MODIS.img.hdr
 cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case4_158.img L8.img
 cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case4_158.hdr L8.hdr
 cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case4_158.bll L8.bll
-cp -uv B11_mar11_screened_USE_THIS_ONE_CSV.csv B11.csv
+cp -uv OpticsModelPerDet11_20140527_RITedits_Y_FLIPPEDCSV.csv B11.csv
 
